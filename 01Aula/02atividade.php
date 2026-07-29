6297660e37ed508c25cd818a8c0985a8c75b8f67
MARIA FERNANDA TROIANI BISCAINO <maria.biscaino2@portalsesisp.org.br>
2026-07-29 12:02:23 -0300

    primeiro commit

---
+++ 01Aula/02atividade.php
@@ -0,0 +1,17 @@
+<?php
+    //Declaração de variável
+    $nome = "Estudande";
+    $idade = "20";
+
+    //Exibindo informações 
+    echo "Olá, $nome!";
+    echo " Você tem $idade anos.";
+
+    //Condicional simples 
+    if ($idade >= 18){
+        echo "Você é maior de idade";
+    }else {
+        echo "Você é menor de idade";
+    }
+
+?>
